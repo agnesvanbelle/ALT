ee425aa6905cf2c46c174771424ae9682439d1fb
Paris Mavromoustakos <parismav87@gmail.com>
2013-10-30 19:17:00 +0100

    fixed some bugs

--- cache.py
+++ cache.py
@@ -1,40 +1,54 @@
 from collections import defaultdict
-import models_utils
+import models_utils 
 from sets import Set
 import os.path
 import subprocess
 
 class Cache(object):
     
-    def __init__(self, e_LMdir, f_LMdir, probs_file, fSen, alpha=0.4, ngram_max_length = 3, topTrans = 20):
+    def __init__(self, e_LMdir, f_LMdir, probs_file, fSen,  e_words_num=20910, f_words_num=34221, alpha=0.4, ngram_max_length = 3, topTrans = 20, delta = 0.0001):
         #here we  load only what is needed for translating  fSen
-        self.f_LMmodel = defaultdict(float) 
+        self.f_LMmodel = defaultdict(float)
         self.e_LMmodel =  defaultdict(float)       
         self.TMmodel = dict()
-        self.LWmodel = defualtdict(float)
+        self.LWmodel = defaultdict(float)
         self.topTrans = topTrans
-        loadmodels(probs_file, fSen, e_LMdir) 
-
         self.alpha = alpha
-        self.ngram_max_length = n_gram_max_length
+        self.ngram_max_length = ngram_max_length
+        self.e_words_num = e_words_num
+        self.f_words_num = f_words_num
         
-    def loadmodels(probs_file, fSen, e_LMdir, f_LMdir):
+        self.loadmodels(probs_file, fSen, e_LMdir, f_LMdir) 
+
+        
+    def loadmodels(self,probs_file, fSen, e_LMdir, f_LMdir):
         #load foreign LM
-        self.f_LMmodel = loadf_LM(f_LMdir, fsen)
+        self.f_LMmodel = self.loadf_LM(f_LMdir, fSen)
         
         e_words_toconsider = Set()
         
-        ngrams = models_utils.get_ngrams(self.ngram_max_length)
+        #get all possible ngrams of the foreign sentence
+        ngrams = models_utils.getngrams(fSen, self.ngram_max_length)
+
         for ngram in ngrams:
-            update_models_fPhrase(ngram, probs_file, e_words_toconsider, TMconsidered)
+            self.update_models_fPhrase(ngram, probs_file, e_words_toconsider)
  
         #load eLM for all possible translations starting with the first word
-        self.e_LMmodel = loade_LM(e_LMdir, list(e_words_toconsider))
+        self.e_LMmodel = self.loade_LM(e_LMdir, list(e_words_toconsider))
         
-    def update_models_fPhrase(fPhrase, probs_file, e_words_toconsider):      
-        command = 'grep ^\"' + fPhrase + ' |||\"' + probs_file
-        result = subprocess.call(command, shell=True)
-        
+    def update_models_fPhrase(self,fPhrase, probs_file, e_words_toconsider):
+        print fPhrase
+        if '<s>' in fPhrase or '</s>' in fPhrase:
+            e_words_toconsider.add('<s>')
+            e_words_toconsider.add('</s>')
+            return   
+        command = 'grep ^\"' + fPhrase + ' |||\" ' + probs_file
+        print fPhrase, command
+        result = subprocess.check_output(command, shell=True)
+
+        print result
+        if result == 1:
+            return
         localTMmodel = dict()
 
         localLWmodel = defaultdict(float)
@@ -72,24 +86,26 @@ class Cache(object):
 
     
     #words_list is all possible words that we might need the english LM prob for    
-    def loade_LM(eLMdir, words_list):
+    def loade_LM(self,eLMdir, words_list):
+        print words_list
         model = defaultdict(float)
         for word in words_list:
-            model.update(loadLMfile(eLMdir, word))
+            model.update(self.loadLMfile(eLMdir, word))
+        print model
         return model
 
-    def loadf_LM(fLMdir, fsen):
+    def loadf_LM(self,fLMdir, fsen):
         model = defaultdict(float)
         toload = Set()
-        for word in fsen.split(' ')
+        for word in fsen.split(' '):
             toload.add(word)
-        for word in toLoad:
-            model.update(loadLMfile(fLMdir, word))
+        for word in toload:
+            model.update(self.loadLMfile(fLMdir, word))
         return model
             
-    def loadLMfile(LMdir, word):
+    def loadLMfile(self,LMdir, word):
         model = defaultdict(float)
-        path = LMdir+'/'+models_utils.java_string_hashcode(word)+'.lm'
+        path = LMdir+'/'+str(models_utils.java_string_hashcode(word))+'.lm'
         
         #check if file exists
         if not os.path.exists(path):
@@ -99,7 +115,7 @@ class Cache(object):
         for line in f_in:
             split = line.split('\t')
             ngram = split[0]
-            prob = float(split[1].replace('\n','')
+            prob = float(split[1].replace('\n',''))
             #add it to the model
             model[ngram] = prob
         f_in.close()
@@ -107,15 +123,18 @@ class Cache(object):
 
     #get stupid back off probability
     #model is the aggregated model loaded from the files. can be either e_LMmodel or f_LMmodel
-    def stupidback_prob(ngram, model):
+    def stupidback_prob(ngram, model, total_words_num):
         if ngram in model:
             return model[ngram]
+        elif len(ngram.split(' ')) == 1:  #not in vocabulary
+            return delta / delta*total_words_num
+            
         split = ngram.split(' ')
         back_ngram = ''
         if len(split)==2:
-            back_ngram = ngram[0]
-        else if len(split) == 3:
-            back_ngram = ngram[0] + ' ' + ngram[1]
+            back_ngram = ngram[1]
+        elif len(split) == 3:
+            back_ngram = ngram[1] + ' ' + ngram[2]
         
         return alpha * stupidback_prob(back_ngram, model)
 
@@ -128,14 +147,32 @@ class Cache(object):
         
   #      return [("to go", 0.32), ("avoiding to", 0.1), ("miss", 0.4)]
 
+    def LM(self, sen, LMmodel, lang_words_num):
+        prob = 0
+        split = sen.split(' ')
+        
+        #<s> first_word
+        prob += stupidback_prob(split[0]+ ' ' + split[1], LMmodel, lang_words_num)
+        
+        #<s> first_word second_word
+        for i in range(2, len(split)):
+            prob += stupidback_prob(split[i-2]+ ' ' + split[i-1] + ' ' +split[i], LMmodel, lang_words_num)
+        return prob
+
     def LMe(self, eSen):
-        #TODO: compute this for a sentence
-        return 0.1
+        return LM(self, eSen, e_LMmodel, e_words_num)
 
     def LMf(self, fSen):
-        #TODO: compute this for a sentence
-        return 0.1
+        return LM(self, fSen, f_LMmodel, f_words_num)
 
     def LW(self, fPhrase, ePhrase):
-        #return 0.1
-        return LWmodel[(fPhrase, ePhrase)]    
+        return LWmodel[(fPhrase, ePhrase)]  
+      
+      
+e_LMdir = '/Users/parismavromoustakos/Desktop/nikos/lm_en/en.lm'
+f_LMdir = '/Users/parismavromoustakos/Desktop/nikos/lm_nl/nl.lm'
+probs_file = '/Users/parismavromoustakos/Desktop/nikos/final_file.txt'
+fSen = 'hello duisternis mijn oude vriend'
+cache = Cache(e_LMdir, f_LMdir, probs_file, fSen) 
+
+#cache.LMf()
